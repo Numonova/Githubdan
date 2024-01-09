8e8f5d297ee4aa3f167808ec03aaabe4505b9d7d
E`zoza <numonovaezoza377@gmail.com>
2024-01-09 21:05:46 +0500

    3 kod

--- Program.cs
+++ Program.cs
@@ -1,37 +1,14 @@
-﻿using System;
-
-public class BaseConverter
+﻿string binaryNumber = "1010"; // Ikkilik sanoq sistemasidagi son
+int decimalNumber = 0;
+int baseValue = 1;
+for (int i = binaryNumber.Length - 1; i >= 0; i--)
 {
-    public static void Main()
+    if (binaryNumber[i] == '1')
     {
-        string binaryNumber = "1010";
-        int decimalNumber = Convert.ToInt32(binaryNumber, 2);
-
-        Console.WriteLine($"Ikkilik son: {binaryNumber} o`nlikka o`tkazilganda: {decimalNumber}");
-
-        decimalNumber = 12;
-        binaryNumber = Convert.ToString(decimalNumber, 2);
-
-        Console.WriteLine($"O`nlikdagi son: {decimalNumber} ikkilikka o`tkazilganda: {binaryNumber}\n");
-
-        string octalNumber = "16";
-        decimalNumber = Convert.ToInt32(octalNumber, 8);
-
-        Console.WriteLine($"Sakkizlik son: {octalNumber} o`nlikka o`tkazilganda: {decimalNumber}");
-
-        decimalNumber = 26;
-        octalNumber = Convert.ToString(decimalNumber, 8);
-
-        Console.WriteLine($"O`nlikdagi son: {decimalNumber} sakkizlikka o`tkazilganda: {octalNumber}\n");
-
-        string hexadecimalNumber = "1A";
-        decimalNumber = Convert.ToInt32(hexadecimalNumber, 16);
-
-        Console.WriteLine($"O'n olchamdagi son: {hexadecimalNumber} o`nlikka o`tkazilganda: {decimalNumber}");
-
-        decimalNumber = 42;
-        hexadecimalNumber = Convert.ToString(decimalNumber, 16).ToUpper();
-
-        Console.WriteLine($"O`nlikdagi son: {decimalNumber} o'n olchamdagi soniga o`tkazilganda: {hexadecimalNumber}\n");
+        decimalNumber += baseValue;
     }
+    baseValue *= 2;
 }
+Console.WriteLine(decimalNumber); // O'nlik sanoq sistemasidagi sonni konsolda chiqarish
+
+
